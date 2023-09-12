a4503a28a605598324255575d8e4f67da9929a49
Pedro Lobato <Pedrosslobato@gmail.com>
2023-09-12 09:00:46 -0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="container">
+        <h2>Sua lista de animes mais completas do mundo</h2>
+        <h3>Ola senhor admin</h3>
+      </div>
+    )
+  }
