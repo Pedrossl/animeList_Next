626cc4702a63376149e2fcb68ff5556b5c63dbc7
Pedro Lobato <pedrosslobato@gmail.com>
2023-09-12 12:15:34 -0300

    Atualizado

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -1,18 +1,22 @@
 export default function Title({ children }) {
-return(
-<body >
-
-<header className="flex justify-between items-center bg-gray-800 py-4 px-6">
-  <div className="text-left">
-    <h1 className="text-2xl font-semibold text-orange-600">AnimeCom</h1>
-  </div>
-  <div className="text-right">
-    <a href="#" className="mr-4 text-gray-200 hover:text-stone-400">Administador</a>
-    <a href="#" className="text-gray-200 hover:text-stone-400 mr-4">Lista de Animes</a>
-    <a href="#" className=" text-gray-200 hover:text-stone-400">Login</a>
-  </div>
-</header>
-
-</body>
-)
+  return (
+    <body>
+      <header className="flex justify-between items-center bg-gray-800 py-4 px-6">
+        <div className="flex items-center">
+          <img src="../Assunto.png" className="w-14 h-14 rounded-full" />
+          <h1 className="text-2xl font-semibold text-orange-600 ml-2">
+            AnimeCom
+          </h1>
+        </div>
+        <div className="text-right">
+          <a href="#" className="text-gray-200 hover:text-stone-400 mr-4">
+            Lista de Animes
+          </a>
+          <a href="#" className="text-gray-200 hover:text-stone-400">
+            Login
+          </a>
+        </div>
+      </header>
+    </body>
+  );
 }
