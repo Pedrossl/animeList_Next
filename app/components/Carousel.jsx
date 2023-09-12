8f0e9b22e7711cc9e328ed23481be921268de9ca
Pedro Lobato <pedrosslobato@gmail.com>
2023-09-12 10:18:45 -0300

    Comitei

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -2,5 +2,15 @@
 import React, { useState } from 'react';
 
 export default function Carousel({ images }) {
-  return
+  const slides = [
+    {url: "https://picsum.photos/id/57..jpeg" },
+    { url: "https://i.imgur.com/vzfGn1ob.jpg" },
+    { url: "https://i.imgur.com/zmXLgvW.gif" },
+    {url: "https://i.imgur.com/1z2ZqZQ.jpg" },
+  ]
+  return(
+    <div className='max-w-[1400px] h-[780px] w-full' >
+      
+    </div>
+  )
 }
