0397ac04555f3c27f7635bcf14bd4cffdf1fb836
Pedro Lobato <pedrosslobato@gmail.com>
2023-09-20 00:24:56 -0300

    logen....

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Login = () => {
   return (
@@ -8,20 +8,30 @@ const Login = () => {
         <h2 className="text-2xl font-bold mb-4 text-orange-800">Login</h2>
         <form>
           <div className="mb-4">
-            <label htmlFor="nome" className="block text-gray-800 text-sm font-bold mb-2">Nome</label>
+            <label
+              htmlFor="nome"
+              className="block text-gray-800 text-sm font-bold mb-2"
+            >
+              Nome
+            </label>
             <input
               type="text"
               id="nome"
-              className="border rounded w-full py-2 px-3"
+              className="border rounded w-full py-2 px-3 text-orange-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="senha" className="block text-gray-800 text-sm font-bold mb-2">Senha</label>
+            <label
+              htmlFor="senha"
+              className="block text-gray-800 text-sm font-bold mb-2"
+            >
+              Senha
+            </label>
             <input
               type="password"
               id="senha"
-              className="border rounded w-full py-2 px-3"
+              className="border rounded w-full py-2 px-3 text-orange-500"
               required
             />
           </div>
@@ -29,13 +39,9 @@ const Login = () => {
             type="submit"
             className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mr-3"
           >
-            
-           <Link href='/'>Logen</Link> 
+            <Link href="/">Login</Link>
           </button>
-          <a
-            href="#"
-            className="text-orange-500 hover:underline"
-          >
+          <a href="#" className="text-orange-500 hover:underline">
             Esqueci minha senha
           </a>
         </form>
